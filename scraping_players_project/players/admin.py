de0a607e6ba856246eba88b616d47fdfb3a2928e
Shubham Mukund Lalge <96787417+shubhamlalge@users.noreply.github.com>
2023-07-18 18:38:16 +0530

    Delete scraping_players_project directory

--- scraping_players_project/players/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Player, City, Class, Committed, School, State, Position, Offer
-
-# Register your models here.
-models = [Player, City, Class, Committed, State, School, Position, Offer]
-
-admin.site.register(models)
