845632f90931c3964bd53fd528d59d143e7b92b1
Shubham Mukund Lalge <96787417+shubhamlalge@users.noreply.github.com>
2023-07-19 18:45:59 +0530

    Add files via upload

---
+++ scraping_players_project/scraping_repo/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from players.views import PlayerViewSet, ClassViewSet, SchoolViewSet, StateViewSet, PositionViewSet, CommitmentViewSet, \
+    OfferViewSet, CityViewSet
+
+router = DefaultRouter()
+
+router.register('players', PlayerViewSet, basename='players')
+router.register('class', ClassViewSet, basename='class')
+router.register('school', SchoolViewSet, basename='school')
+router.register('state', StateViewSet, basename='state')
+router.register('position', PositionViewSet, basename='position')
+router.register('commitment', CommitmentViewSet, basename='commitment')
+router.register('offer', OfferViewSet, basename='offer')
+router.register('city', CityViewSet, basename='city')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('users.urls')),
+    path('api/', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+]
