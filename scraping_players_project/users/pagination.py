902973a95f622b7af29ca024caa04fdca11f5cb4
Shubham Mukund Lalge <96787417+shubhamlalge@users.noreply.github.com>
2023-07-27 17:57:39 +0530

    Delete scraping_players_project directory

--- scraping_players_project/users/pagination.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class CustomPagination(PageNumberPagination):
-    ''' This class is used for custom pagination '''
-    page_size = 5
